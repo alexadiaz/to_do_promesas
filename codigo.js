97d48212bae685e6bfd6e02c2ec20f51b67ba5b7
Alexa <alexadiaz@msn.com>
2017-08-22 21:31:40 -0400

    leer parametros en linea de comando

--- codigo.js
+++ codigo.js
@@ -2,6 +2,15 @@ const readline = require("readline");
 var mysql = require("promise-mysql");
 const rl = leer_pantalla();
 
+if(process.argv.length > 2){
+    iniciar(process.argv[2]);
+}
+else{
+    rl.question("Que accion desea realizar:",function(accion){
+        iniciar(accion);
+    });    
+}
+
 function leer_pantalla(){
     const rl= readline.createInterface({
         input:process.stdin,
